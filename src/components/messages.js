21fb0baf6079cea82568accbff9c33c98fd19595
Rupa <rupa211984@gmail.com>
2019-11-18 21:51:28 -0800

    Done

---
+++ src/components/messages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Messages = () => (
+  <div>
+    <p>I am message 1</p>
+    <p>I am message 2</p>
+  </div>
+);
+
+export default Messages;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import About from './components/about';
+import Login from './components/login';
+import Signup from './components/signup';
+import Messages from './components/messages';
+import Navbar from './components/navbar';
 
 const Home = () => {
   return (
@@ -10,6 +17,17 @@ const Home = () => {
 };
 
 ReactDOM.render(
-  <Home />,
+  (<Router>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/messages" component={Messages} />
+      </Switch>
+    </div>
+  </ Router>),
   document.getElementById('root')
 );
